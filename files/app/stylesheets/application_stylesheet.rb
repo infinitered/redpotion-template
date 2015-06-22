d33b129f69f9eb4b3758bf144a71869f714815d7
Todd Werth <todd@infinitered.com>
2015-06-22 14:23:40 -0700

    Added first version of appearance. This should be cleaned up some

--- files/app/stylesheets/application_stylesheet.rb
+++ files/app/stylesheets/application_stylesheet.rb
@@ -23,6 +23,8 @@ class ApplicationStylesheet < RubyMotionQuery::Stylesheet
     color.add_named :tint, '236EB7'
     color.add_named :translucent_black, color(0, 0, 0, 0.4)
     color.add_named :battleship_gray,   '#7F7F7F'
+
+    StandardAppearance.apply app.window
   end
 
   def standard_button(st)
